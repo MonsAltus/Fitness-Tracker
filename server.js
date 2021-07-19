074446fa8140540d0c5486221dd64451964f79ad
James <James.Hacklander@gmail.com>
2021-07-19 14:01:13 -0700

    Created server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const routes = require('./controllers');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+// app.use(require('./routes'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static('public'));
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
